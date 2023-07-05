38da31fe26fdb187a98b0dbb36641d4350f7bf91
EmanIftikhar <93860459+EmanIftikhar@users.noreply.github.com>
2023-07-05 21:16:26 +0400

    more changes

--- app.py
+++ app.py
@@ -89,5 +89,5 @@ def pdfToText(file):
         text += page.extract_text()
     return text
 
-if __name__ == "_main_":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
