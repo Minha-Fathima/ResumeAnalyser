222251aaf731a923e7039c42f93de9c0430bdead
Minha Fathima Avoor <113610393+Minha-Fathima@users.noreply.github.com>
2023-07-06 15:42:54 +0530

    Create Certificate_Recommendation_System.py

---
+++ Certificate_Recommendation_System.py
@@ -0,0 +1,77 @@
+# model ID: efc339ef-ad88-43ff-b090-966477dd49b3
+import ai21
+import os
+import requests
+import json
+
+from dotenv import load_dotenv
+
+# Load environment variables from .env.local file
+load_dotenv(".env.local")
+
+# Access the API key
+api_key = os.getenv("API_KEY")
+
+ai21.api_key = api_key
+
+
+def certificateRecommendation(jobTitle):
+    prompt = f"Job Title: {jobTitle}\nCertification:"
+    print(prompt)
+
+    url = (
+        "https://api.ai21.com/studio/v1/j2-mid/Certificate_Recommendation_Sys/complete"
+    )
+
+    payload = {
+        "numResults": 3,
+        "maxTokens": 200,
+        "minTokens": 0,
+        "temperature": 0.7,
+        "topP": 1,
+        "topKReturn": 0,
+        "frequencyPenalty": {
+            "scale": 1,
+            "applyToWhitespaces": True,
+            "applyToPunctuations": True,
+            "applyToNumbers": True,
+            "applyToStopwords": True,
+            "applyToEmojis": True,
+        },
+        "presencePenalty": {
+            "scale": 0,
+            "applyToWhitespaces": True,
+            "applyToPunctuations": True,
+            "applyToNumbers": True,
+            "applyToStopwords": True,
+            "applyToEmojis": True,
+        },
+        "countPenalty": {
+            "scale": 0,
+            "applyToWhitespaces": True,
+            "applyToPunctuations": True,
+            "applyToNumbers": True,
+            "applyToStopwords": True,
+            "applyToEmojis": True,
+        },
+        "prompt": prompt,
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": f"Bearer {api_key}",
+    }
+    response = requests.post(url, json=payload, headers=headers)
+
+    response_text = response.text
+    response_json = json.loads(response_text)
+
+    completion = response_json["completions"][0]
+    output_text = completion["data"]["text"]
+
+    print(output_text)
+    return output_text
+
+# test case
+jobTitle = "Data Science"
+certificateRecommendation(jobTitle)
